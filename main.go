d31228b6ac7030387e9ee5aade84523e3cb46148
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-05-10 14:18:39 -0400

    add database logic to main pacakge

--- main.go
+++ main.go
@@ -1,6 +1,58 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
+
+const (
+	host     = "localhost"
+	port     = 8080
+	user     = "mueed"
+	password = "P@ssw0rd!"
+	dbname   = "phone_number_normalizer"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmobile=disable", host, port, user, password)
+	must(phonedb.Reset("postgres", psqlInfo, dbname))
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	must(phonedb.Migrate("postgres", psqlInfo))
+
+	db, err := phonedb.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	err = db.Seed()
+	must(err)
+
+	phones, err := db.AllPhones()
+	must(err)
+	for _, p := range phones {
+		fmt.Printf("Working on... %+v\n", p)
+		number := normalize(p.Number)
+		if number != p.Number {
+			fmt.Println("Updating or removing...", number)
+			existing, err := db.FindPhone(number)
+			must(err)
+			if existing != nil {
+				must(db.DeletePhone(p.ID))
+			} else {
+				p.Number = number
+				must(db.UpdatePhone(&p))
+			}
+		} else {
+			fmt.Println("No changes required")
+		}
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func normalize(phone string) string {
 	var buf bytes.Buffer
